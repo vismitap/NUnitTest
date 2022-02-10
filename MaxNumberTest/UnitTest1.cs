a73871b64b14a515176eb7c9caf4678da7686544
vismitap <vismita.prabhu@gmail.com>
2022-02-11 00:17:37 +0530

    [Vismita] Add: Folder structure added

---
+++ MaxNumberTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace MaxNumberTest
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
